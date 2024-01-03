d682ed53f42bd62e95b3c271d463fdca4bb906d5
juan.rompani <juan.rompani@gmail.com>
2024-01-03 00:25:04 -0300

    * Adding Price Controller

---
+++ src/main/java/com/app/hexarchitecture/capitole/infrastructure/adapters/in/controller/price/model/Error.java
@@ -0,0 +1,7 @@
+package com.app.hexarchitecture.capitole.infrastructure.adapters.in.controller.price.model;
+
+import lombok.Builder;
+
+@Builder
+public record Error(String code, String error) {
+}
---
+++ src/main/java/com/app/hexarchitecture/capitole/infrastructure/adapters/in/controller/price/model/Response.java
@@ -0,0 +1,27 @@
+package com.app.hexarchitecture.capitole.infrastructure.adapters.in.controller.price.model;
+
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record Response<T,E>(LocalDateTime timeStamp, T data, E error, boolean success) {
+
+    public static <T, E> ResponseEntity<Response<T, E>> ok(T data) {
+        return ResponseEntity.ok(Response.<T, E>builder().success(true).data(data).build());
+    }
+
+    public static <T, E> ResponseEntity<Response<T, E>> created(T data) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(Response.<T, E>builder().success(true).data(data).build());
+    }
+
+    public static <T, E> ResponseEntity<Response<T, E>> error(E error, HttpStatus httpStatus) {
+        return ResponseEntity.status(httpStatus).body(Response.<T, E>builder().success(false).error(error).build());
+    }
+
+    public ResponseEntity<Response<T, E>> toResponseEntity(HttpStatus status) {
+        return new ResponseEntity<>(this, status);
+    }
+}
